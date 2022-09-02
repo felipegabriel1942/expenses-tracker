5319519a294328a2691d4c74197d6449df78dbbb
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2022-09-02 18:16:50 -0300

    Implementando primeng

--- src/app/@modules/home/home.module.ts
+++ src/app/@modules/home/home.module.ts
@@ -2,8 +2,10 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
   declarations: [HomeComponent],
@@ -11,6 +13,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     HomeRoutingModule,
     ReactiveFormsModule,
+    InputTextModule,
+    PanelModule
   ],
 })
 export class HomeModule {}
