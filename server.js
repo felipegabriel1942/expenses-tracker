e930b860dcbd3e171ad154c6b083ea4b34cd43bd
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2023-07-07 21:08:02 -0300

    Corrigindo caminho da aplicação na pasta dist

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/expense-tracker'));
+app.use(express.static(__dirname + '/dist/expenses-tracker'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/expense-tracker/index.html'));});
+'/dist/expenses-tracker/index.html'));});
 app.listen(process.env.PORT || 8080);
