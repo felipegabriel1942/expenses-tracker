b9c5ca98dba17716d89af9c010d41c2dab81f62d
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2023-07-11 19:07:40 -0300

    Criando menu com dropdown no header

--- src/app/@core/components/header/header.component.ts
+++ src/app/@core/components/header/header.component.ts
@@ -1,5 +1,7 @@
+import { DeviceDetectorService } from 'ngx-device-detector';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private route: Router) {}
+  items: MenuItem[] | undefined;
+  isMobile: boolean;
 
-  public ngOnInit(): void {}
+  constructor(
+    private readonly route: Router,
+    private readonly deviceDetectorService: DeviceDetectorService
+  ) {}
+
+  public ngOnInit(): void {
+    this.isMobile = this.deviceDetectorService.isMobile();
+    this.items = [
+      {
+        label: 'Sair',
+        icon: 'pi pi-fw pi-power-off',
+        command: () => this.logout(),
+      },
+    ];
+  }
 
   public logout(): void {
     sessionStorage.removeItem('token');
--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { MessageInterceptor } from './interceptors/message.interceptor';
 import { RequestInterceptor } from './interceptors/request.interceptor';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, TieredMenuModule, ButtonModule, AvatarModule],
   exports: [HeaderComponent],
   declarations: [HeaderComponent],
   providers: [
